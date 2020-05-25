372f6d3ea1db1da8de16df11eace80b21dae48dc
jundongsi <sijundong@outlook.com>
2020-05-25 21:56:21 +0800

    add interval

--- src/math/Bounds.h
+++ src/math/Bounds.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "ScalarArray.h"
+
+namespace Dash
+{
+	namespace Math
+	{
+		template<typename Scalar, std::size_t N>
+		class Bounds
+		{
+		public:
+			Bounds();
+			~Bounds();
+
+		private:
+
+		};
+	}
+}
