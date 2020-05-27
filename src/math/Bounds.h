0cd7b187a5839de75219727cf4133a5eaab1d6bf
jundongsi <sijundong@outlook.com>
2020-05-27 21:27:25 +0800

    modified bounds

--- src/math/Bounds.h
+++ src/math/Bounds.h
@@ -20,13 +20,51 @@ namespace Dash
 			ScalarArray<Scalar, N> Upper() const noexcept;
 			ScalarArray<Scalar, N>& Upper() noexcept;
 			
-			ScalarArray<Scalar, N> Lerp() noexcept;
-			ScalarArray<Scalar, N> Offset() noexcept;
-			void BoundingSphere() noexcept;
+			ScalarArray<Scalar, N> Lerp(const ScalarArray<Scalar, N>& t) noexcept;
+			ScalarArray<Scalar, N> Offset(const ScalarArray<Scalar, N>& p) noexcept;
+			void BoundingSphere(ScalarArray<Scalar, N>& center, Scalar& radius) noexcept;
 
 		private:
 			ScalarArray<Scalar, N> mLower;
 			ScalarArray<Scalar, N> mUpper;
 		};
+
+		template<typename Scalar, std::size_t N> ScalarArray<Scalar, N> Lower(const Bounds<Scalar, N>& b) noexcept;
+		template<typename Scalar, std::size_t N> ScalarArray<Scalar, N> Upper(const Bounds<Scalar, N>& b) noexcept;
+
+		template<typename Scalar, std::size_t N> ScalarArray<Scalar, N> Center(const Bounds<Scalar, N>& b) noexcept;
+
+		template<typename Scalar, std::size_t N> Bounds<Scalar, N> Union(const ScalarArray<Scalar, N>& p1,
+			const ScalarArray<Scalar, N>& p2) noexcept;
+
+		template<typename Scalar, std::size_t N> Bounds<Scalar, N> Union(const Bounds<Scalar, N>& b,
+			const ScalarArray<Scalar, N>& p) noexcept;
+
+		template<typename Scalar, std::size_t N> Bounds<Scalar, N> Union(const ScalarArray<Scalar, N>& p,
+			const Bounds<Scalar, N>& b) noexcept;
+
+		template<typename Scalar, std::size_t N> Bounds<Scalar, N> Union(const Bounds<Scalar, N>& b1,
+			const Bounds<Scalar, N>& b2) noexcept;
+
+		template<typename Scalar, std::size_t N> Bounds<Scalar, N> Intersect(const Bounds<Scalar, N>& b1,
+			const Bounds<Scalar, N>& b2) noexcept;
+
+		template<typename Scalar, std::size_t N> bool Overlaps(const Bounds<Scalar, N>& b1,
+			const Bounds<Scalar, N>& b2) noexcept;
+
+		template<typename Scalar, std::size_t N> bool Inside(const ScalarArray<Scalar, N>& p,
+			const Bounds<Scalar, N>& b) noexcept;
+
+		template<typename Scalar, std::size_t N> bool InsideExclusive(const ScalarArray<Scalar, N>& p,
+			const Bounds<Scalar, N>& b) noexcept;
+
+		template<typename Scalar, std::size_t N> bool Expand(const Bounds<Scalar, N>& p,
+			Scalar delta) noexcept;
+
+		template<typename Scalar, std::size_t N> bool Distance(const ScalarArray<Scalar, N>& p,
+			const Bounds<Scalar, N>& b) noexcept;
+
+		template<typename Scalar, std::size_t N> bool DistanceSquared(const ScalarArray<Scalar, N>& p,
+			const Bounds<Scalar, N>& b) noexcept;
 	}
 }
